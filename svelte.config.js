e65abc1fc85f9badaaa021caac32b16e7f188e77
mohrazzak <mohrazzak7@gmail.com>
2024-04-23 17:36:15 +0300

    vercel edits

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
   kit: {
     adapter: adapter({
-      runtime: 'nodejs18.x',
+      runtime: 'edge',
     }),
   },
   preprocess: vitePreprocess(),
